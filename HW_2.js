586e888621e753470654fa4cb1409e673c9353dd
Elena Kanash <hird250@gmail.com>
2023-03-30 19:28:06 +0300

    add homework tasks

---
+++ HW_2.js
@@ -0,0 +1,5 @@
+/* 1. Имеется зашифрованное предложение
+“Ybrobrubr brabrrbrebr brtbrhbrebr brbbrebrsbrtbr brQbrAbr brebrvbrebrrbr”
+мы знаем, что оно означает “You are the best QA ever”.
+Нам нужно придумать программу дешифратор.
+ */
---
+++ HW_5.js
@@ -0,0 +1,15 @@
+/* 1.Напишите функцию  калькулятор , в которую, в качестве параметров, передаются три переменные, две - числовые и третья отвечает за знак арифметической операции (*, /, -, +). Проверьте работу функции.
+example:  function calculate(a, b, operator){} */
+
+/* 2. Напишите пошаговый алгоритм приготовления кофе и выведите его на экран (страницу).
+Requirements for Coffee
+When user first come he should see a welcome message
+There are three types of cup sizes
+User should be able to see cup prices for each size before an  order
+There are two recipes at this time, we should able to update code if added new recipe
+Show to a user the name and the  ingredients of each type of coffee before an order
+User should be able to make an order, by choosing cup size and coffee type
+When a choice is made, calculate the price based on cup size. Show steps for the recipe and order price.
+If user is lactose free, remove milk step from the recipe
+
+Details: price, names of coffee, ingredients, cup sizes, recipe, welcome message details or text, final message,do we have confirm option,  payment method, how to calc price, business type, recipe for lactose free option, what milk choice do we have?, sugar Is or Not, */
