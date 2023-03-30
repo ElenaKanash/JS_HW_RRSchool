111c1aa4b50dfd35e8953e991fc18ae7076e14f5
Elena Kanash <hird250@gmail.com>
2023-03-30 19:57:59 +0300

    add HW_5 task solution

--- HW_5.js
+++ HW_5.js
@@ -1,6 +1,37 @@
 /* 1.Напишите функцию  калькулятор , в которую, в качестве параметров, передаются три переменные, две - числовые и третья отвечает за знак арифметической операции (*, /, -, +). Проверьте работу функции.
 example:  function calculate(a, b, operator){} */
 
+function sum(num1, num2) {
+  return num1 + num2
+}
+
+function substract(num1, num2) {
+  return num1 - num2
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+function divide(num1, num2) {
+  if (num2 == 0) return "You can't divide by zero. Please enter another number.";
+  return num1 / num2
+}
+
+function calc(num1, num2, sign) {
+  let result = 0;
+  if (isNaN(num1) || isNaN(num2)) result = 'Please, enter a number'
+  else if (sign == '+') result = sum(num1, num2)
+  else if (sign == '-') result = substract(num1, num2)
+  else if (sign == '*') result = multiply(num1, num2)
+  else if (sign == '/') result = divide(num1, num2)
+  else result = 'Please enter a right sign';
+
+  return result
+}
+
+console.log(calc(10, 5, '$'));
+
 /* 2. Напишите пошаговый алгоритм приготовления кофе и выведите его на экран (страницу).
 Requirements for Coffee
 When user first come he should see a welcome message
