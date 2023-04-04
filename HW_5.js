9f06e8549d4c0a2d1a08745d860831cbdecffb86
Elena Kanash <hird250@gmail.com>
2023-04-04 18:57:01 +0300

    add solution to HW_5

--- HW_5.js
+++ HW_5.js
@@ -30,7 +30,8 @@ function calc(num1, num2, operator) {
   return result
 }
 
-console.log(calc(10, 5, '$'));
+//console.log(calc(10, 5, '$'));
+//console.log(calc(10, 5, '+'));
 
 /* 2. Напишите пошаговый алгоритм приготовления кофе и выведите его на экран (страницу).
 Requirements for Coffee
@@ -44,3 +45,100 @@ When a choice is made, calculate the price based on cup size. Show steps for the
 If user is lactose free, remove milk step from the recipe
 
 Details: price, names of coffee, ingredients, cup sizes, recipe, welcome message details or text, final message,do we have confirm option,  payment method, how to calc price, business type, recipe for lactose free option, what milk choice do we have?, sugar Is or Not, */
+
+function showWelcomMessage() {
+  console.log('Welcom to our self-service cafe');
+  console.log('We have different cup size. Please, choose one: \n');
+}
+//console.log(showWelcomMessage());
+
+function showCupsOption() {
+  let smallCup = 'small cup';
+  let mediumCup = 'medium cup';
+  let largeCup = 'large cup';
+  let price = 5;
+
+  let cupsOption = [[smallCup, price], [mediumCup, (price * 1.5)], [largeCup, (price * 2)]];
+
+  for (let i = 0; i<cupsOption.length; i++) {
+    console.log(cupsOption[i][0] + ' ' + '-' +  ' ' + cupsOption[i][1] + ' '  + '$');
+    if (i === cupsOption.length - 1 ) {
+      console.log('\n');
+    }
+  }
+}
+//console.log(showCapOption());
+
+function showCoffeTypesAndIngridients() {
+  let latte = 'Latte';
+  let icedCoffee = 'Iced Coffee';
+  let beansArabica = 'Arabica ground beans';
+  let beansEthiopian = 'Ethiopian ground beans';
+  let hotWater = 'hot water';
+  let coldWater = 'cold water';
+  let latteIngredients = `${latte}: \n 1 spoon of ${beansArabica} \n ${hotWater} \n milk \n`;
+  let IcedCoffeeIngredients = `${icedCoffee}: \n 1 spoon of ${beansEthiopian} \n ${coldWater} \n milk \n`;
+
+  console.log('We have following coffee types. Please, choose one: \n');
+
+  let menu = [latteIngredients, IcedCoffeeIngredients]
+
+  for (let i = 0; i<menu.length; i++) {
+    console.log(menu[i]);
+  }
+}
+//console.log(showCoffeTypesAndIngridients());
+
+function setWaterForCoffee(coffeeType) {
+  if (coffeeType.toLowerCase() == 'iced coffee') {
+    return water = 'cold water'
+  } else {
+    return water = 'hot water';
+  }
+}
+//console.log(setWaterForCoffee('cold water'))
+
+function getPrice(cupSize) {
+  let price = 5;
+  if (cupSize == 'small') {
+    return price
+  } else if (cupSize == 'medium') {
+    return price *= 1.5
+  } else if (cupSize == 'large') {
+    return price *= 2
+  }
+}
+//console.log(getPrice('small'));
+
+function showRecipeToClient(cupSize, coffeeType,isLactoseFree, milk = 'half&half milk') {
+  let water = setWaterForCoffee(coffeeType);
+  let price = getPrice(cupSize);
+  let order = `You choosed a ${cupSize} cup of ${coffeeType.toLowerCase()} \n Follow the steps: `;
+  let step1 = ` Take a ${cupSize} cup`;
+  let step2 = ` Put 1 spoon of coffeee in it`;
+  let step3 = ` Pour ${water} in a cup with coffee`;
+  let step4 = ` Pour ${milk} in a cup with coffee`;
+  let step5 = ` Stir coffee in a cup with a spoon`;
+  let orderPrice = `The order price is ${price}`;
+  let result = `Enjoy your coffee`;
+
+  let recipe = [step1, step2, step3, step4, step5];
+  let stepNum = 1;
+  let clientRecipe = '';
+
+  for (let i = 0; i< recipe.length; i++) {
+    if (isLactoseFree == true && recipe[i] == step4) continue;
+    clientRecipe = clientRecipe + (stepNum++) + recipe[i] + '\n';
+  }
+
+  console.log(order + '\n' + clientRecipe + '\n' + orderPrice + '\n' + result);
+
+}
+
+showWelcomMessage();
+showCupsOption();
+showCoffeTypesAndIngridients();
+
+showRecipeToClient('small', 'latte', false, 'coconut milk');
+showRecipeToClient('small', 'latte', false);
+showRecipeToClient('small', 'latte', true);
